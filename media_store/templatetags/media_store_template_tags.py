75914291b820414f8c88410b4e11d742d739e038
Craig Dick <2378675D@student.gla.ac.uk>
2020-03-27 19:06:05 +0000

    Added User authentication and header

--- media_store/templatetags/media_store_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from media_store.models import Category
-
-register = template.Library()
-
-@register.inclusion_tag('media_store/categories.html')
-def get_category_list(current_category=None):
-    return {'categories': Category.objects.all(),
-            'current_category': current_category}
