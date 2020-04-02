1c5585f1874af06d6da903756698e26bb2ba3068
Craig Dick <2378675D@student.gla.ac.uk>
2020-04-02 15:16:40 +0100

    tidied templates

--- populate_mobile_store.py
+++ populate_mobile_store.py
@@ -4,7 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import django
 django.setup()
-from mobile_store.models import Category, Page
+
 from mobile_store.models import Item
 
 def populate():
