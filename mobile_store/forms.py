b0500c7af06671444a7cfb9e2b63232968d05cac
theunusedbrick <62437668+theunusedbrick@users.noreply.github.com>
2020-04-03 11:48:27 +0100

    Includes the checkout form

--- mobile_store/forms.py
+++ mobile_store/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-
+from django import views
 from django.contrib.auth.models import User
-from mobile_store.models import Contact, Review
+from mobile_store.models import Contact
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
 
 #form for the contact to submit their feedback using
 class ContactForm(forms.ModelForm):
@@ -22,17 +25,32 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password',)
 
-class ReviewForm(forms.ModelForm):
- 
-    name = forms.CharField(max_length=Contact.NAME_MAX_LENGTH, label="Name")
-    phone = forms.CharField(max_length=Review.PHONE_MAX_LENGTH,label="Phone")
-    review = forms.CharField(label='', max_length=Review.REVIEW_MAX_LENGTH, widget=forms.Textarea(attrs={'placeholder': 'Enter your review here'}))
-    rating = forms.IntegerField(label="Rating")
 
-    class Meta:
-        model = Review
-        fields = ('name', 'phone', 'review', 'rating',)
-
+#checkout page form for rediriecting to payment method
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+class CheckoutForm(forms.Form):
+    street_address = forms.CharField()
+    apartment_address = forms.CharField(required=False)
+    country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={'class':'custom-select d-block w-100'}))
+    zip = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    same_shipping_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
